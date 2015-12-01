d43848ea89a83e3589455e9e17de3ad4e1f4fa0c
Robert Morris <obviouslyrob@gmail.com>
2015-12-01 17:11:22 +0000

    Added the devise gem and followed through its documentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Yelpster::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'https://yelpster-rhgm.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
